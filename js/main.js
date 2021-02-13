ff45125f5275d67f2b12b744f6b651eca0ac11d7
Dr. Alberto Cruz <albert@odin.cs.csubak.edu>
2021-02-13 13:51:38 -0800

    Switch to using document on ready

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
-#("form_id").submit(function(event){
-	event.preventDefault();
-	submitForm();
+$(document).ready(function(){
+    console.log( "JS on ready function has loaded." );
+    // Button that should call Ajax to check the password user has submitted
+    $("button").click(function(){
+        console.log( "????" );
+    });
+    // Prevent form from resetting the page during debugging ... do I need to remove this???
+    $("#form_id").submit(function(e) {
+            e.preventDefault();
+    });
 });
-
-function submitForm() {
-	var password = $("password_id").val();
-
-	alert( "Password is" + password );
-}
